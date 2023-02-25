b552f7433518d6ee721c359ccafde34656ced752
sanjanar06 <sanjanareji06@gmail.com>
2023-02-25 14:10:20 +0530

    Changed viseme frames name format

--- Components/Viseme_Extraction/viseme.py
+++ Components/Viseme_Extraction/viseme.py
@@ -1,46 +1,46 @@
 import cv2
 import numpy as np
 
-def extract_frames(video_file, timestamp_ranges):
-    # Load the video
-    video = cv2.VideoCapture(video_file)
-
-    # Get the video properties
-    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+def extract_frames(video_path, timestamp_ranges):
+    # Open the video file
+    video = cv2.VideoCapture(video_path)
+    
+    # Get the frame rate and total number of frames in the video
     frame_rate = video.get(cv2.CAP_PROP_FPS)
-
-    # Initialize an empty list to store the extracted frames
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    # Initialize the list of frames
     frames = []
-
+    
     # Loop through the timestamp ranges
-    for start_stamp, end_stamp in timestamp_ranges:
-        # Calculate the frame numbers for the given start and end timestamps
-        start_frame = int(start_stamp * frame_rate)
-        end_frame = int(end_stamp * frame_rate)
-
-        # Check if the frame numbers are within the range of the video
-        if end_frame >= total_frames:
-            print("Error: Frame number out of range")
-            continue
-
-        # Calculate the frame interval between each extracted frame
-        num_frames = end_frame - start_frame + 1
-        frame_interval = max(num_frames // 6, 1)
-
-        # Loop through the range of frame numbers and extract frames at intervals of the frame interval
-        for i in range(start_frame, end_frame + 1, frame_interval):
+    for i, timestamp_range in enumerate(timestamp_ranges):
+        start_time = timestamp_range[0]
+        end_time = timestamp_range[1]
+        
+        # Calculate the frames to extract
+        frames_to_extract = np.linspace(start_time, end_time, 6)
+        
+        for j, frame_time in enumerate(frames_to_extract):
+            # Calculate the frame number for the given time-stamp
+            frame_number = int(frame_time * frame_rate)
+        
+            # Check if the frame number is within the range of the video
+            if frame_number >= total_frames:
+                print("Error: Frame number out of range")
+                continue
+            
             # Set the video position to the desired frame
-            video.set(cv2.CAP_PROP_POS_FRAMES, i)
-
+            video.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+            
             # Read the frame
             _, frame = video.read()
-
-            # Add the frame to the list
-            frames.append(frame)
-
-    # Release the video
+            
+            # Append the frame to the list with timestamp_index and frame_sequencing number
+            frames.append((frame,i,j))
+    
+    # Release the video object and return the frames list
     video.release()
-
-    # Return the list of extracted frames
     return frames
+
+
 
